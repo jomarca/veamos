ae97276e0ac84d4016e1cf2710801174405f7315
Jose Manuel Martinez Carrion <jomarca23@gmail.com>
2019-04-14 19:11:45 +0200

    filter asd asas

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static fi les (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
